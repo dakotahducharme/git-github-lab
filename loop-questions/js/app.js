d8c73acd5f86ee7608b2a9b5950d0d7266de8d21
Dakotah Ducharme <dakotahbducharme@gmail.com>
2018-12-11 22:38:16 -0800

    Commit 9- answered all loop questions

---
+++ loop-questions/js/app.js
@@ -0,0 +1,63 @@
+
+let i = 0;
+
+while (i <= 99){
+  console.log(i);
+  i++;
+}
+
+i = 99;
+while (i >= 0){
+  console.log(i);
+  i--;
+}
+
+i = 298;
+while (i >= 1){
+  console.log(i);
+  i--;
+  i--;
+  i--;
+}
+
+i = 81;
+while (i >= 26){
+  console.log(i);
+  i--;
+}
+
+i = 49;
+while (i <= 72){
+  console.log(i);
+  i++;
+}
+
+i = 81;
+while (i >= 26){
+  console.log(i);
+  i--;
+}
+
+i = 97;
+while (i >= 0){
+  if i % 2 === 1 {
+    console.log(i);
+  }
+  i--;
+}
+
+i = 0;
+while (i <= 98){
+  if i % 2 === 0 {
+    console.log(i);
+  }
+  i++;
+}
+
+i = 1;
+while (i <= 91){
+  if i % 3 === 0 {
+    console.log(i);
+  }
+  i++;
+}
